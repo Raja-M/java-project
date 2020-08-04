296e71ff978448a3605be50dd914751fd7f460ce
rmudiga <raja.mudiganti@optum.com>
2020-08-04 13:16:45 -0500

    Intial

---
+++ Learning/Basics/VarOperFlow.java
@@ -0,0 +1,22 @@
+
+class VaroperFlow{
+
+	byte bt = 0;
+	short srt = 0;
+	int i = 0;
+	long lg = 0;
+	float flt = 0.0f;
+	double db = 0.0d;
+	
+	char ch = ' ';
+
+	String  str = 'Test string';
+
+	boolean bl = true;
+
+
+
+
+
+}
+	
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ testing again
 demo
 New change by Raja
 one more change by Raja
+New line added
