957d145d0c69da78a86aa62b1a8d6d192d412d03
rmudiga <raja.mudiganti@optum.com>
2020-08-04 15:43:15 -0500

    folders

---
+++ learning/basics/VarOperFlow.java
@@ -0,0 +1,29 @@
+package learning.basics;
+
+
+public class VarOperFlow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+ 
+				byte bt = 0;
+				short srt = 0;
+				int i = 0;
+				long lg = 0;
+				float flt = 0.0f;
+				double db = 0.0d;
+				
+				char ch = ' ';
+			
+				String  str = "Test string";
+			
+				boolean bl = true;
+				
+				System.out.println("Hellow World !");
+ 
+
+
+	}
+
+}
